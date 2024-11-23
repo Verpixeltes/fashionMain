12b0629c802bdd0126e5e9924fce8f952e2486f6
Verpixeltes <namid.sheehan.rau@bkhaspel.de>
2024-11-23 20:24:28 +0100

    first push

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['uovuxazyhwrpucwwiyjg.supabase.co'],
     },
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    }
 };
 
 export default nextConfig;
